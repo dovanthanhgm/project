29d7735da0584732f556667b34aa13b37e843028
dovanthanhgm <149997073+dovanthanhgm@users.noreply.github.com>
2023-12-16 04:46:56 +0000

    up

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 from flask import Flask, request, jsonify, make_response, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 
@@ -9,7 +10,10 @@ db = SQLAlchemy(app)
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String)
-    def json(self): return {'id': self.id, 'title': self.title}
+    description = db.Column(db.Text)
+    image = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    def json(self): return {'id': self.id, 'title': self.title, 'description': self.description, 'image': self.image, 'created_at': self.created_at}
 with app.app_context(): db.create_all()
 ##################################################
 api = Blueprint('api', __name__)
@@ -23,7 +27,7 @@ def add():
     try: data = request.get_json()
     except: data = request.form
     if data:
-        product = Product(title=data['title'])
+        product = Product(title=data['title'], description=data['description'], image=data['image'])
         db.session.add(product)
         db.session.commit()
         return make_response(jsonify({'message': 'success'}), 201)
@@ -33,26 +37,35 @@ app.register_blueprint(api, url_prefix='/api')
 from flask import render_template_string
 shop = Blueprint('shop', __name__)
 @shop.route('/home')
-def index():
+def home():
     products = Product.query.all()
     return render_template_string(
         """
         <!DOCTYPE html><html><head>
         <style>
-            table, th, td {border-collapse: collapse;border: 1px solid black;}
+            body {width: 75%; margin: 1rem auto; border-collapse: collapse; border: 1px solid black;} ul, li {list-style-type: none} li {display: inline-block; margin-right: 10px;} table, ul {width: 90%; margin: 1rem auto} table, th, td {border-collapse: collapse;border: 1px solid black;}
+            
         </style>
         </head>
         <body>
+            <ul>
+                <li><a href="{{ url_for('shop.home') }}">Home</a></li>
+                <li><a href="{{ url_for('shop.add') }}">Add</a></li>
+            </ul> <hr>
             <table><thead><tr>
                 <th>ID</th>
                 <th>Title</th>
                 <th>Description</th>
+                <th>Image</th>
+                <th>CreatedAt</th>
             </tr></thead><tbody>
                 {% for product in products %}
                 <tr>
                     <td>{{ product.id }}</td>
                     <td>{{ product.title }}</td>
-                    <td> Lorem ipsum. </td>
+                    <td> {{ product.description }} </td>
+                    <td> <img src="{{ product.image }}" style="width: 50px; height: 50xp;"/> </td>
+                    <td> {{ product.created_at.strftime("%d/%m/%Y") }} </td>
                 </tr>
                 {% endfor %}
             </tbody></table>
@@ -60,12 +73,27 @@ def index():
         """,
         products=products
     )
+@shop.route('/add')
+def add():
+    return render_template_string(
+        """
+        <!DOCTYPE html><html><head>
+        <style>
+        </style>
+        </head>
+        <body>
+            <form>
+                <input type="text">
+            </form>
+        </body></html>
+        """,
+    )
 app.register_blueprint(shop, url_prefix='/shop')
 ##################################################
 from flask import render_template_string
 shop_v2 = Blueprint('shop_v2', __name__)
 @shop_v2.route('/home')
-def index():
+def home():
     products = Product.query.all()
     return render_template_string(
         """
